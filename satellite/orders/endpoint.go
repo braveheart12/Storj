634c4c72751d265b0b3ea7889275475e04e8b828
Egon Elbre <egonelbre@gmail.com>
2019-07-12 16:28:37 -0400

    rename package pkg/certdb to satellite/certdb

--- satellite/orders/endpoint.go
+++ satellite/orders/endpoint.go
@@ -16,10 +16,10 @@ import (
 	"gopkg.in/spacemonkeygo/monkit.v2"
 
 	"storj.io/storj/pkg/auth/signing"
-	"storj.io/storj/pkg/certdb"
 	"storj.io/storj/pkg/identity"
 	"storj.io/storj/pkg/pb"
 	"storj.io/storj/pkg/storj"
+	"storj.io/storj/satellite/certdb"
 )
 
 // DB implements saving order after receiving from storage node
